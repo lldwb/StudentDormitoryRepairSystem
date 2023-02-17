3f1397d924a73b3e119c32d687bb2dfd6296cfd1
lldwb <3247187440@qq.com>
2023-02-17 10:10:52 +0800

    Changes

--- src/main/java/com/entity/FilmInfo.java
+++ src/main/java/com/entity/FilmInfo.java
@@ -2,8 +2,52 @@ package com.entity;
 
 public class FilmInfo {
     private int mid;
-    private        mname
-    private room
-    private        context
-    private status
+
+    public int getMid() {
+        return mid;
+    }
+
+    public void setMid(int mid) {
+        this.mid = mid;
+    }
+
+    public String getMname() {
+        return mname;
+    }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public boolean isContext() {
+        return context;
+    }
+
+    public void setContext(boolean context) {
+        this.context = context;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private String mname;
+    private String room;
+    private boolean context;
+    private String status;
+
+    public FilmInfo() {
+    }
 }
